ca3bc3144eab38c83f36f86c10c937abfee206bb
WILLIAM_CHUANG <zhuang8026@gmail.com>
2021-05-03 18:04:33 +0800

    [mbp16] push

--- src/imooc_class/Imooc_2_02.java
+++ src/imooc_class/Imooc_2_02.java
@@ -5,14 +5,16 @@ public class Imooc_2_02 {
     public static void main(String[] args) {
         String s1 = "imooc";
         String s2 = "imooc";
+        String s22 = new String("imooc");
         String s3 = "I love" + s1;
         //定义字符串s3，保存“I love”和s1拼接后的内容
 
 
         // 比较字符串s1和s2
         // imooc为常量字符串，多次出现时会被编译器优化，只创建一个对象
-        System.out.println("s1和s2内存地址相同吗？" + (s1 == s2));
-        System.out.println("s1和s2內容相同吗？" + (s1.equals(s2)));
+        System.out.println("s1和s2内存地址相同吗？-1" + (s1 == s2));
+        System.out.println("s1和s2內容相同吗？-2" + (s1.equals(s2)));
+        System.out.println("s1和s22內容相同吗？-3" + (s1 == s22));
 
         //比较字符串s1和s3
         System.out.println("s1和s3内存地址相同吗？" + (s1 == s3));
